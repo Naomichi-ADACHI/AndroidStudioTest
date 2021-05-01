d45a590b2c4ad13c650c6f2b50bfe441dd591dbb
Naomichi Adachi <naoyk1212@yahooc.o.jp>
2021-05-02 00:05:34 +0900

    [UPDATE] 画面に日付を出力するよう変更

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView = textView = findViewById(R.id.helloText);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        textView.setText("Hello " + new Date().toString());
+
         setContentView(R.layout.activity_main);
     }
 
