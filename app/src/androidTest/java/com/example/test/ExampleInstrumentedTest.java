f188b8ff5eac303add8418fe84a6e15d17654323
yudai5511m <yudai5511m@gmail.com>
2021-05-01 23:44:35 +0900

    コミットしてみるだけ

--- app/src/androidTest/java/com/example/test/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/test/ExampleInstrumentedTest.java
@@ -23,4 +23,6 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.test", appContext.getPackageName());
     }
+
+    //コミットしてみたよ
 }
