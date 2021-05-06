c580b0dcd88367e821cc5d7ca38c4d7f3bf9a547
naoyk1212 <naoyk1212@yahoo.co.jp>
2021-05-06 21:50:50 +0900

    [UPDATE] 表ページのActivityを作成し、遷移ボタンを作成

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -22,6 +23,26 @@ public class MainActivity extends AppCompatActivity {
         View view = binding.getRoot();
 
         binding.dateText.setText(new Date().toString());
+
+        // Updateボタンのハンドラ
+        // 時刻のアップデートを行う
+        binding.dateUpdateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.dateText.setText(new Date().toString());
+            }
+        });
+
+        // Transボタンのハンドラ
+        // TableViewへ遷移
+        binding.transButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), TableActivity.class);
+                startActivity(intent);
+            }
+        });
+
         setContentView(view);
     }
 
