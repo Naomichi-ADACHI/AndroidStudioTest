55115e488219e74e991e5f4c4ea05af2951be4bc
naoyk1212 <naoyk1212@yahoo.co.jp>
2021-05-06 14:44:13 +0900

    [UPDATE] 起動後即落ちる問題を修正

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -3,20 +3,26 @@ package com.example.test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+
+import com.example.test.databinding.ActivityMainBinding;
 
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView textView = textView = findViewById(R.id.helloText);
+    private TextView textView;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        textView.setText("Hello " + new Date().toString());
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
 
-        setContentView(R.layout.activity_main);
+        binding.dateText.setText(new Date().toString());
+        setContentView(view);
     }
 
     //test2
